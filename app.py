b55e07bc8e3813d0601789d727917e98c06c8db8
Reynaldo003 <rvallejo276@gmail.com>
2025-08-12 17:19:46 -0600

    V2.0

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import sett 
 import services
+import os
 
 app = Flask(__name__)
 
@@ -42,4 +43,5 @@ def recibir_mensajes():
         return 'no enviado ' + str(e)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
